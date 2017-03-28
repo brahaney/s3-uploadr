650683176b971f9562ce7930c089568b94819e50
Daniel Brahaney <dbrahaney@dyn.com>
2017-03-28 14:08:43 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # s3-uploadr
 Simple docker S3 uploader
+
+```
+docker run --rm -v $(pwd):/data brahaney/s3-uploadr  \
+--file my-file.jpg \
+--name my-file-in-s3.jpg \
+--bucket my-awesome-bucket \
+--aws_access_key $AWS_ACCESS_KEY \
+--aws_secret_key $AWS_SECRET_KEY \
+--public
+```
---
+++ app/entrypoint.py
@@ -0,0 +1,26 @@
+import tinys3
+import sys
+import argparse
+
+arg_parser = argparse.ArgumentParser()
+
+arg_parser.add_argument("--file", required=True)
+arg_parser.add_argument("--name", required=True)
+arg_parser.add_argument("--bucket", required=True)
+arg_parser.add_argument("--aws_access_key", required=True)
+arg_parser.add_argument("--aws_secret_key", required=True)
+arg_parser.add_argument("--public", action="store_true", default=False)
+
+args = arg_parser.parse_args()
+
+src_filename = sys.argv[1]
+dest_filename = sys.argv[2]
+
+conn = tinys3.Connection(args.aws_access_key,
+                         args.aws_secret_key,
+                         tls=True)
+
+with open("/data/" + args.file, 'rb') as f:
+    r = conn.upload(args.name, f, args.bucket, public=args.public)
+    if r.status_code != 200:
+        raise ConnectionError("{} - {}".format(r.status_code, r.reason))
