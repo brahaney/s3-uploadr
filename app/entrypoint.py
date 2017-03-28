dbb07d004e8dc7a328cf812c7c18ccb9dee71b35
Daniel Brahaney <dbrahaney@dyn.com>
2017-03-28 15:39:01 -0400

    subdir support, multifile support

--- app/entrypoint.py
+++ app/entrypoint.py
@@ -4,8 +4,8 @@ import argparse
 
 arg_parser = argparse.ArgumentParser()
 
-arg_parser.add_argument("--file", required=True)
-arg_parser.add_argument("--name", required=True)
+arg_parser.add_argument("files", nargs="*")
+arg_parser.add_argument("--s3_subdir", default=None)
 arg_parser.add_argument("--bucket", required=True)
 arg_parser.add_argument("--aws_access_key", required=True)
 arg_parser.add_argument("--aws_secret_key", required=True)
@@ -20,8 +20,16 @@ conn = tinys3.Connection(args.aws_access_key,
                          args.aws_secret_key,
                          tls=True)
 
-with open("/data/" + args.file, 'rb') as f:
-    r = conn.upload(args.name, f, args.bucket, public=args.public)
-    if r.status_code != 200:
-        raise ConnectionError("{} - {}".format(r.status_code, r.reason))
-    print("Uploaded file to: " + r.url)
+# add subdirectory to s3 file "key"
+s3_key_format = "{}".format
+print("starting")
+
+if args.s3_subdir is not None:
+    s3_key_format = (args.s3_subdir + "/{}").format
+
+for fname in args.files:  # for each filename in files list
+    with open("/data/" + fname, 'rb') as f:
+        r = conn.upload(s3_key_format(fname), f, args.bucket, public=args.public)
+        if r.status_code != 200:
+            raise ConnectionError("{} - {}".format(r.status_code, r.reason))
+        print("Uploaded file to: " + r.url)
